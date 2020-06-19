fbeba664571faed65ccea4aeb3b78da3436ae7a8
Nikolas Kuschnig <nikolas.kuschnig@wu.ac.at>
2020-06-19 15:21:59 +0200

    Move stuff, regress a bit

--- code/cem-prep.R
+++ code/cem-prep.R
@@ -3,7 +3,7 @@ library("dplyr")
 library("sf")
 library("cem")
 
-path_in <- "data/"
+path_in <- "/mnt/nfs_fineprint/tmp/mining_def/"
 
 prep_country <- function(x, geom = FALSE) {
   if(!isTRUE(geom)) {x$geometry <- NULL}
@@ -35,9 +35,9 @@ treat_country <- function(x, dist_treated, dist_control) {
   return(treated)
 }
 
-job_id <- 4
 files <- list.files(path_in)
-file <- files[[job_id]]
+# file <- files[[1]]
+file <- files[grep("asia-VNM", files)]
 
 x <- readRDS(paste0(path_in, file))
 y <- prep_country(x)
@@ -49,7 +49,7 @@ imb <- imbalance(y$treated, as.data.frame(y),
   drop = c("countries", "id_grid"))
 
 out1 <- cem("treated", y,
-  drop = names(y)[!grepl("elevation|slope|area_forest_2000|dist_road", names(y))],
+  drop = names(y)[!grepl("elevation|slope|area_forest_2000|dist_road|pop_2000|dist_waterway", names(y))],
   keep.all = TRUE)
 
 lm1 <- att(out1, area_accumulated_forest_loss ~
@@ -58,23 +58,10 @@ lm1 <- att(out1, area_accumulated_forest_loss ~
 summary(lm1)
 
 pair1 <- pair(out1, data = y)
-
 out_r <- relax.cem(out1, data = as.data.frame(y))
 
 
-out <- vector("list", length(dist_treated) + 1)
-names(out) <- c("data", names(dist_treated))
-
-for(treated_name in names(dist_treated)) {
-  x[[treated_name]] <- treat_country(x, dist_treated[[treated_name]], dist_control)
-
-  out[[treated_name]] <- if(sum(!is.na(x[[treated_name]])) > 1000) {
-    get_matchit(x, treated_name, models, methods)
-  } else {NULL}
-}
-
-out[["data"]] <- x # Now includes all treatment vars
-
-saveRDS(out, paste0(path_out, "/", file))
-
-# }
+lm2 <- lm(area_accumulated_forest_loss ~
+  poly(distance_mine, 3) + slope + elevation + area_forest_2000 + pop_2000,
+  data = as.data.frame(y), weights = out1$w)
+summary(lm2)
--- code/cem_helper.R
+++ code/cem_helper.R
@@ -3,6 +3,6 @@ cem_data <- function(x) {
 
   if(!inherits(x, "cem.match")) stop("Provide a 'cem.match' object.")
 
-  out <- data.frame(treated = x[["groups"]], weight = x[["w"]])
+  out <- data.frame(cem_treated = x[["groups"]], cem_weight = x[["w"]])
   return(out)
 }
