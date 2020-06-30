0cdc83428d5b724558e0203f2a0a6603f78e4b52
Nikolas Kuschnig <nikolas.kuschnig@wu.ac.at>
2020-06-30 14:40:45 +0200

    Add legend for ESA CCI

--- code/2_analyse.R
+++ code/2_analyse.R
@@ -31,29 +31,33 @@ summary(lm2)
 # Models -----
 
 form1 <- area_accumulated_forest_loss ~
-  poly(distance_mine, 5) + poly(elevation, 5) + poly(slope, 5) +
-  soilgrid + esa_cci_2000 + pop_2000 +
-  poly(area_forest_2000, 5) + poly(dist_road, 5) + dist_waterway +
-  I(elevation * slope) + I(pop_2000 * distance_mine) +
-  I(pop_2000 * area_forest_2000) + I(elevation * slope) +
-  I(elevation * area_forest_2000) + I(slope * area_forest_2000)
+  poly(distance_mine, 3) + poly(elevation, 3) + poly(slope, 3) +
+  poly(pop_2000, 3) + poly(area_forest_2000, 3) +
+  poly(dist_road, 3) + poly(dist_waterway, 3) +
+  # soilgrid + esa_cci_2000 +
+  I(elevation * slope) +
+  I(pop_2000 * distance_mine) + I(pop_2000 * area_forest_2000) +
+  I(area_forest_2000 * elevation) + I(area_forest_2000 * slope) +
+  I(dist_road * pop_2000) + I(dist_road * area_forest_2000)
 
 y <- tbl[["area_accumulated_forest_loss"]]
 X <- model.matrix(form1, data = tbl)[, -1] # Screw the constant
 X <- scale(X)
 
 cv_ridge <- cv.glmnet(x = X, y = y, weights = NULL, alpha = 0)
-out_ridge <- glmnet(x = X, y = y, weights = NULL,
-  alpha = 0, lambda = cv_ridge[["lambda.1se"]])
-coef(out_ridge)
+out_ridge <- glmnet(x = X, y = y, weights = NULL, alpha = 0)
 
-cv_lasso <- cv.glmnet(x = X, y = y, weights = NULL, alpha = 0)
-out_lasso <- glmnet(x = X, y = y, weights = NULL,
-  alpha = 0, lambda = cv_lasso[["lambda.1se"]])
-coef(out_lasso)
+coef(out_ridge, exact = TRUE, s = cv_ridge[["lambda.min"]])
+plot(out_ridge)
+
+cv_lasso <- cv.glmnet(x = X, y = y, weights = NULL, alpha = 1)
+out_lasso <- glmnet(x = X, y = y, weights = NULL, alpha = 1)
+
+coef(out_lasso, exact = TRUE, s = cv_lasso[["lambda.min"]])
+plot(out_lasso)
 
 out_lm <- lm(y ~ X)
 sm(out_lm)
 
 out_bma <- BMS::bms(cbind(y, X), user.int = FALSE)
-sm(out_bma)
+print(out_bma)
