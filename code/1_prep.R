c8e34cabb8ad886d18f8c6319f0fa9dc736e3ecd
Nikolas Kuschnig <nikolas.kuschnig@wu.ac.at>
2020-07-08 15:13:49 +0000

    Edit models a bit

--- code/1_prep.R
+++ code/1_prep.R
@@ -25,22 +25,6 @@ file <- files[grep("sou.*ECU", files)]
 
 for(file in files) {
 
-# library("parallel")
-# cl <- makeCluster(detectCores() - 1)
-# parLapply(cl, files, function(file) {
-
-  # Stuff for para
-  library("dplyr")
-  library("sf")
-  source("code/9_helpers.R")
-  countries <- read.csv("input/countries.csv")
-  path_in <- "/mnt/nfs_fineprint/tmp/mining_def/"
-  if(!dir.exists(path_in)) {
-    path_in <- "data/"
-    if(!dir.exists(path_in)) {stop("Feed me data!")}
-  }
-
-
   cat("Running for ", get_iso(file), ".\n", sep = "")
 
   tbl_raw <- readRDS(paste0(path_in, file))
@@ -55,37 +39,33 @@ for(file in files) {
   tbl$treated_farer <- calc_treatment(tbl,
     dist_treated = c(-1, 2e5), dist_control = 2e5)
 
+  # match_on <- c("elevation", "slope", "area_forest_2000", "pop_2000",
+  #   "dist_waterway", "soilgrid_grouped", "esa_cci_2000_grouped")
   match_on <- c("elevation", "slope", "area_forest_2000", "pop_2000",
-    "dist_waterway", "soilgrid_grouped", "esa_cci_2000_grouped")
+    "dist_waterway", "soilgrid_grouped", "esa_cci_2000", "ecoregions_2017")
   source("code/2_cem.R")
 
   # Models
   formulas <- list(
-    "form1" = area_accumulated_forest_loss ~
-      distance_mine + I(distance_mine * treated) +
-      I(distance_mine * treated_far) + I(distance_mine * treated_farer) +
-      elevation + slope +
-      pop_2000 + area_forest_2000 +
-      dist_road + I(distance_mine * dist_road) +
-      dist_waterway + I(distance_mine * dist_waterway) +
-      distance_protected_area + distance_cropland_2000 +
-      soilgrid_grouped + esa_cci_2000_grouped,
-    "f_no-road" = area_accumulated_forest_loss ~
+    "f_base" = area_accumulated_forest_loss ~
       distance_mine + I(distance_mine * treated) +
       elevation + slope +
       pop_2000 + area_forest_2000 +
+      dist_road +
       dist_waterway +
-      distance_protected_area + distance_cropland_2000 +
+      distance_protected_area + 
+      distance_cropland_2000 +
       soilgrid_grouped + esa_cci_2000_grouped,
-    "f_no-pa" = area_accumulated_forest_loss ~
+    "f_interactions" = area_accumulated_forest_loss ~
       distance_mine + I(distance_mine * treated) +
-      elevation + slope +
+      elevation + slope + I(elevation * slope) +
       pop_2000 + area_forest_2000 +
-      dist_road + dist_waterway + distance_cropland_2000 +
+      dist_road + I(distance_mine * dist_road) + I(dist_waterway * dist_road) +
+      dist_waterway +
+      distance_protected_area + I(distace_protected_area * dist_road) +
+      distance_cropland_2000 + I(distance_cropland_2000 * dist_road) +
       soilgrid_grouped + esa_cci_2000_grouped)
+  
   source("3_models.R")
-
-# })
-# stopCluster(cl)
 
 }
