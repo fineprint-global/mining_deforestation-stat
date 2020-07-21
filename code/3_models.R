3e941e344e2331eb769d98e0f44bac6aa0159d8a
SLuckeneder <sebastian.luckeneder@wu.ac.at>
2020-07-21 08:37:08 +0000

    mute unweighted model, fix bug counting lasso_n_0, log y

--- code/3_models.R
+++ code/3_models.R
@@ -11,7 +11,7 @@ for(i in seq(formulas)) {
 
 # Models -----
 
-y <- tbl[["area_accumulated_forest_loss"]]
+y <- log(tbl[["area_accumulated_forest_loss"]]+1) # add small constant for taking log
 X <- model.matrix(formulas[[i]], data = tbl)[, -1] # no need for constant here
 X <- scale(X)
 X <- X[, !apply(X, 2, function(x) all(is.na(x)))]
@@ -22,8 +22,8 @@ cat("Fitting models with least squares.\n")
 
 out_lm1 <- lm(y ~ X, weights = out_cem[["w"]])
 summary(out_lm1)
-out_lm2 <- lm(y ~ X, weights = NULL)
-summary(out_lm2)
+# out_lm2 <- lm(y ~ X, weights = NULL)
+# summary(out_lm2)
 
 # Penalized regression -----
 
@@ -42,10 +42,14 @@ dev.off()
 readr::write_csv(tibble(
   "vars" = c("constant", colnames(X)),
   "lm_coef" = coef(out_lm1),
-  "lm_unweighted_coef" = coef(out_lm2),
+  "lm_se" = sqrt(diag(vcov(out_lm1))),
+  # "lm_unweighted_coef" = coef(out_lm2),
+  # "lm_unweighted_se" = sqrt(diag(vcov(out_lm2))),
   "lasso_cv_coef" = (coef(out_lasso, exact = TRUE,
     s = cv_lasso[["lambda.min"]]))[, 1],
-  "lasso_n_0" = apply(coef(out_lasso), 1, function(x) sum(x < 0.01))),
+  "lasso_n_0" = apply(coef(out_lasso), 1, function(x) sum(x < 0.01 & x > -0.01)),
+  "country" = rep(get_iso(file), length(coef(out_lm1))),
+  "model" = rep(names(formulas)[i], length(coef(out_lm1)))),
   path = paste0("output/txt/coef_", get_iso(file), "_",
     names(formulas)[i], ".csv"))
 
@@ -57,10 +61,11 @@ readr::write_csv(tibble(
   "N_tr_matched" = out_cem[["tab"]][2, 2],
   "N_untr_unmatched" = out_cem[["tab"]][2, 1],
   "BIC" = BIC(out_lm1),
-  "BIC_unweighted" = BIC(out_lm2),
+  # "BIC_unweighted" = BIC(out_lm2),
   "R2" = summary(out_lm1)$r.squared),
   path = paste0("output/txt/info_", get_iso(file), "_",
     names(formulas)[i], ".csv"))
 
-rm(X, y, out_lm1, out_lm2, out_lasso, cv_lasso); gc()
+# rm(X, y, out_lm1, out_lm2, out_lasso, cv_lasso); gc()
+rm(X, y, out_lm1, out_lasso, cv_lasso); gc()
 }
