546cecdd2de447e2e646cc165e45cca41ef9efaa
Nikolas Kuschnig <nikolas.kuschnig@wu.ac.at>
2020-07-30 15:17:34 +0200

    Clean up code a bit

--- code/1_prep.R
+++ code/1_prep.R
@@ -22,11 +22,13 @@ if(!dir.exists(path_in)) {
 # files <- paste0(countries$continent, "-", countries$iso, ".rds")
 files <- c("south_america-BRA.rds", "oceania-IDN.rds", "oceania-MYS.rds")
 
-file <- files[[1]]
+file <- files[[2]]
 # file <- files[grep("NIC", files)]
 
 # Store CEM outputs
 STORE_CEM <- TRUE
+CALC_LASSO <- TRUE
+SCALE_CENTER <- TRUE
 
 for(file in files) {
 
@@ -39,38 +41,17 @@ for(file in files) {
   # Add variables
   tbl <- add_vars(tbl,
     treated = c(-1, 5e4),
-    dist_log = TRUE, dist_bool = 5000, dist_decay = 0.5)
+    dist_log = TRUE,
+    dist_bool = TRUE, # _bool at 1km, then suffixed
+    dist_decay = 0.5)
 
   # CEM
   match_on <- c("elevation", "slope", "area_forest_2000", "pop_2000",
     "dist_waterway", "soilgrid_grouped", "esa_cci_2000", "ecoregions_2017")
+
   source("code/2_cem.R")
-  
-  # # Analyse data: fit splines distance mine vs forest loss
-  # data <- tbl
-  # xs <- seq(0, max(data[["distance_mine"]]), length.out = 1000)
-  # 
-  # out_spl <- lm(area_accumulated_forest_loss ~
-  #                 bs(distance_mine), data = data, weights = out_cem[["w"]])
-  # smpl <- sample(nrow(data), min(10000, nrow(data) / 4))
-  # 
-  # pred <- predict(out_spl, newdata = list(distance_mine = xs))
-  # png(paste0("output/plots/spline_matched/",
-  #            sub(".*([A-Z]{3}).rds", "\\1", file), ".png"), width = 960, height = 720)
-  # plot(x = data[["distance_mine"]][smpl],
-  #      y = data[["area_accumulated_forest_loss"]][smpl],
-  #      col = "grey", xlab = "Distance to mine", ylab = "Forest loss",
-  #      main = paste0("Spline matched - ", gsub(".*([A-Z]{3}).rds", "\\1", file)),
-  #      ylim = c(0, min(max(pred) * 5, max(data[["area_accumulated_forest_loss"]]))))
-  # points(xs, pred, col = "darkgreen", lwd = 2, type = "l")
-  # grid()
-  # dev.off()
-  # rm(data, out_spl); gc()
 
   # Models
-  
-  # further: base_decay model
-  
   formulas <- list(
     "f_base" = area_accumulated_forest_loss_log ~
       distance_mine +
@@ -170,8 +151,8 @@ for(file in files) {
       distance_protected_area_decay +
       distance_cropland_2000_decay +
       soilgrid_grouped + esa_cci_2000
-    )
+  )
 
   source("code/3_models.R")
-  
+
 }
--- code/3_models.R
+++ code/3_models.R
@@ -4,7 +4,7 @@
 
 # Dependencies -----
 
-library("glmnet") # LASSO
+if(CALC_LASSO) {library("glmnet")}
 
 if(!exists("tbl")) {stop("Please prepare the data in `tbl`.")}
 if(!exists("out_cem")) {stop("Please provide matched data in `out_cem`.")}
@@ -17,61 +17,56 @@ for(i in seq(formulas)) {
 
   y <- tbl[[as.character(formulas[[i]][[2]])]]
   X <- model.matrix(formulas[[i]], data = tbl)[, -1] # no need for constant here
-  X <- scale(X)
+  if(SCALE_CENTER) {X <- scale(X)}
   X <- X[, !apply(X, 2, function(x) all(is.na(x)))]
 
 
   cat("Fitting models with least squares.\n")
 
-
   out_lm1 <- lm(y ~ X, weights = out_cem[["w"]])
   summary(out_lm1)
-  # out_lm2 <- lm(y ~ X, weights = NULL)
-  # summary(out_lm2)
 
   # Penalized regression -----
-  cat("Fitting penalised models.\n")
+  if(CALC_LASSO) {
+    cat("Fitting penalised models.\n")
 
-  cv_lasso <- cv.glmnet(x = X, y = y, weights = out_cem[["w"]], alpha = 1)
-  out_lasso <- glmnet(x = X, y = y, weights = out_cem[["w"]], alpha = 1)
+    cv_lasso <- cv.glmnet(x = X, y = y, weights = out_cem[["w"]], alpha = 1)
+    out_lasso <- glmnet(x = X, y = y, weights = out_cem[["w"]], alpha = 1)
+  }
 
   # Outputs -----
 
-  # png(paste0("output/plots/lasso_simple",
-  #   get_iso(file), ".png"), width = 960, height = 720)
-  # plot(out_lasso, label = TRUE)
-  # dev.off()
+  # if(CALC_LASSO) {
+  #   png(paste0("output/plots/lasso_simple",
+  #     get_iso(file), ".png"), width = 960, height = 720)
+  #   plot(out_lasso, label = TRUE)
+  #   dev.off()
+  # }
 
-readr::write_csv(tibble(
-  "vars" = c("constant", colnames(X)),
-  "lm_coef" = coef(out_lm1),
-  "lm_se" = sqrt(diag(vcov(out_lm1))),
-  # "lm_unweighted_coef" = coef(out_lm2),
-  # "lm_unweighted_se" = sqrt(diag(vcov(out_lm2))),
-  "lasso_cv_coef" = (coef(out_lasso, exact = TRUE,
-    s = cv_lasso[["lambda.min"]]))[, 1],
-  "lasso_n_0" = apply(coef(out_lasso), 1, function(x) sum(x < 0.01 & x > -0.01)),
-  "country" = rep(get_iso(file), length(coef(out_lm1))),
-  "model" = rep(names(formulas)[i], length(coef(out_lm1)))),
-  path = paste0("output/txt/coef_", get_iso(file), "_",
-    names(formulas)[i], ".csv"))
+  readr::write_csv(tibble(
+    "vars" = c("constant", colnames(X)),
+    "lm_coef" = coef(out_lm1),
+    "lm_se" = sqrt(diag(vcov(out_lm1))),
+    # "lasso_cv_coef" = (coef(out_lasso, exact = TRUE,
+    #   s = cv_lasso[["lambda.min"]]))[, 1],
+    # "lasso_n_0" = apply(coef(out_lasso), 1, function(x) sum(abs(x) > 0.01)),
+    "country" = rep(get_iso(file), length(coef(out_lm1))),
+    "model" = rep(names(formulas)[i], length(coef(out_lm1)))),
+    path = paste0("output/txt/coef_", get_iso(file), "_",
+      names(formulas)[i], ".csv"))
 
-readr::write_csv(tibble(
-  "country" = get_iso(file),
-  "model" = names(formulas)[i],
-  "N" = sum(out_cem[["w"]]),
-  "N_full" = nrow(tbl),
-  "N_treated" = out_cem[["tab"]][1, 2],
-  "N_untreated" = out_cem[["tab"]][1, 1],
-  "N_tr_matched" = out_cem[["tab"]][2, 2],
-  "N_untr_unmatched" = out_cem[["tab"]][2, 1],
-  "BIC" = BIC(out_lm1),
-  # "BIC_unweighted" = BIC(out_lm2),
-  "R2" = summary(out_lm1)$r.squared),
-  path = paste0("output/txt/info_", get_iso(file), "_",
-    names(formulas)[i], ".csv"))
-
-# rm(X, y, out_lm1, out_lm2, out_lasso, cv_lasso); gc()
-rm(X, y, out_lm1, out_lasso, cv_lasso); gc()
+  readr::write_csv(tibble(
+    "country" = get_iso(file),
+    "model" = names(formulas)[i],
+    "N" = sum(out_cem[["w"]]),
+    "N_full" = nrow(tbl),
+    "N_treated" = out_cem[["tab"]][1, 2],
+    "N_untreated" = out_cem[["tab"]][1, 1],
+    "N_tr_matched" = out_cem[["tab"]][2, 2],
+    "N_untr_unmatched" = out_cem[["tab"]][2, 1],
+    "BIC" = BIC(out_lm1),
+    "R2" = summary(out_lm1)$r.squared),
+    path = paste0("output/txt/info_", get_iso(file), "_",
+      names(formulas)[i], ".csv"))
 
 }
