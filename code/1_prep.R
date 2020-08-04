f1354b9c5b371c726ed87544bbcd8d6b318db7d5
SLuckeneder <sebastian.luckeneder@wu.ac.at>
2020-08-04 13:13:31 +0000

    add non-linear model, i.e. interaction dummies for distances

--- code/1_prep.R
+++ code/1_prep.R
@@ -23,11 +23,11 @@ if(!dir.exists(path_in)) {
 files <- c("south_america-BRA.rds", "oceania-IDN.rds", "oceania-MYS.rds")
 
 file <- files[[2]]
-# file <- files[grep("NIC", files)]
+# file <- files[grep("LBR", files)]
 
 # Store CEM outputs
 STORE_CEM <- TRUE
-CALC_LASSO <- TRUE
+CALC_LASSO <- FALSE
 SCALE_CENTER <- TRUE
 
 for(file in files) {
@@ -150,7 +150,74 @@ for(file in files) {
       dist_waterway_decay +
       distance_protected_area_decay +
       distance_cropland_2000_decay +
+      soilgrid_grouped + esa_cci_2000,
+    "f_base_log_nonlins" = area_accumulated_forest_loss_log ~
+      distance_mine_log + I(distance_mine_log * distance_mine_km5) + I(distance_mine_log * distance_mine_km10) + I(distance_mine_log * distance_mine_km20)+ I(distance_mine_log * distance_mine_km50) +
+      elevation + slope +
+      pop_2000_log + area_forest_2000_log +
+      dist_road_log + I(dist_road_log * dist_road_km5) + I(dist_road_log * dist_road_km10) + I(dist_road_log * dist_road_km20)+ I(dist_road_log * dist_road_km50) +
+      dist_waterway_log + I(dist_waterway_log * dist_waterway_km5) + I(dist_waterway_log * dist_waterway_km10) + I(dist_waterway_log * dist_waterway_km20)+ I(dist_waterway_log * dist_waterway_km50) +
+      distance_protected_area_log + I(distance_protected_area_log * distance_protected_area_km5) + I(distance_protected_area_log * distance_protected_area_km10) + I(distance_protected_area_log * distance_protected_area_km20)+ I(distance_protected_area_log * distance_protected_area_km50) +
+      distance_cropland_2000_log + I(distance_cropland_2000_log * distance_cropland_2000_km5) + I(distance_cropland_2000_log * distance_cropland_2000_km10) + I(distance_cropland_2000_log * distance_cropland_2000_km20)+ I(distance_cropland_2000_log * distance_cropland_2000_km50) +
+      soilgrid_grouped + esa_cci_2000,
+    "f_base_bool_log_nonlins" = area_accumulated_forest_loss_log ~
+      distance_mine_bool + distance_mine_log + I(distance_mine_log * distance_mine_km5) + I(distance_mine_log * distance_mine_km10) + I(distance_mine_log * distance_mine_km20)+ I(distance_mine_log * distance_mine_km50) +
+      elevation + slope +
+      pop_2000_log + area_forest_2000_log +
+      dist_road_bool + dist_road_log + I(dist_road_log * dist_road_km5) + I(dist_road_log * dist_road_km10) + I(dist_road_log * dist_road_km20)+ I(dist_road_log * dist_road_km50) +
+      dist_waterway_bool + dist_waterway_log + I(dist_waterway_log * dist_waterway_km5) + I(dist_waterway_log * dist_waterway_km10) + I(dist_waterway_log * dist_waterway_km20)+ I(dist_waterway_log * dist_waterway_km50) +
+      distance_protected_area_bool + distance_protected_area_log + I(distance_protected_area_log * distance_protected_area_km5) + I(distance_protected_area_log * distance_protected_area_km10) + I(distance_protected_area_log * distance_protected_area_km20)+ I(distance_protected_area_log * distance_protected_area_km50) +
+      distance_cropland_2000_bool + distance_cropland_2000_log + I(distance_cropland_2000_log * distance_cropland_2000_km5) + I(distance_cropland_2000_log * distance_cropland_2000_km10) + I(distance_cropland_2000_log * distance_cropland_2000_km20)+ I(distance_cropland_2000_log * distance_cropland_2000_km50) +
+      soilgrid_grouped + esa_cci_2000,
+    "f_base_log_interactions_nonlins" = area_accumulated_forest_loss_log ~
+      distance_mine_log + I(distance_mine_log * distance_mine_km5) + I(distance_mine_log * distance_mine_km10) + I(distance_mine_log * distance_mine_km20)+ I(distance_mine_log * distance_mine_km50) +
+      elevation + slope + I(elevation * slope) +
+      pop_2000_log + area_forest_2000_log + I(pop_2000_log * area_forest_2000_log) +
+      I(dist_road_log * pop_2000_log) + I(dist_road_log * distance_cropland_2000_log) +
+      dist_road_log + I(dist_road_log * dist_road_km5) + I(dist_road_log * dist_road_km10) + I(dist_road_log * dist_road_km20)+ I(dist_road_log * dist_road_km50) +
+      dist_waterway_log + I(dist_waterway_log * dist_waterway_km5) + I(dist_waterway_log * dist_waterway_km10) + I(dist_waterway_log * dist_waterway_km20)+ I(dist_waterway_log * dist_waterway_km50) +
+      distance_protected_area_log + I(distance_protected_area_log * distance_protected_area_km5) + I(distance_protected_area_log * distance_protected_area_km10) + I(distance_protected_area_log * distance_protected_area_km20)+ I(distance_protected_area_log * distance_protected_area_km50) +
+      distance_cropland_2000_log + I(distance_cropland_2000_log * distance_cropland_2000_km5) + I(distance_cropland_2000_log * distance_cropland_2000_km10) + I(distance_cropland_2000_log * distance_cropland_2000_km20)+ I(distance_cropland_2000_log * distance_cropland_2000_km50) +
+      soilgrid_grouped + esa_cci_2000,
+    "f_sub_5_20_log_nonlins" = area_accumulated_forest_loss_log ~
+      distance_mine_log + I(distance_mine_log * distance_mine_km5) + I(distance_mine_log * distance_mine_km20) +
+      elevation + slope +
+      pop_2000_log + area_forest_2000_log +
+      dist_road_log + I(dist_road_log * dist_road_km5) +  I(dist_road_log * dist_road_km20)+
+      dist_waterway_log + I(dist_waterway_log * dist_waterway_km5) + I(dist_waterway_log * dist_waterway_km20)+
+      distance_protected_area_log + I(distance_protected_area_log * distance_protected_area_km5) + I(distance_protected_area_log * distance_protected_area_km20)+
+      distance_cropland_2000_log + I(distance_cropland_2000_log * distance_cropland_2000_km5) +  I(distance_cropland_2000_log * distance_cropland_2000_km20)+
+      soilgrid_grouped + esa_cci_2000,
+    "f_sub_5_20_log_interactions_nonlins" = area_accumulated_forest_loss_log ~
+      distance_mine_log + I(distance_mine_log * distance_mine_km5) + I(distance_mine_log * distance_mine_km20)+
+      elevation + slope + I(elevation * slope) +
+      pop_2000_log + area_forest_2000_log + I(pop_2000_log * area_forest_2000_log) +
+      I(dist_road_log * pop_2000_log) + I(dist_road_log * distance_cropland_2000_log) +
+      dist_road_log + I(dist_road_log * dist_road_km5) +  I(dist_road_log * dist_road_km20)+
+      dist_waterway_log + I(dist_waterway_log * dist_waterway_km5) +  I(dist_waterway_log * dist_waterway_km20)+
+      distance_protected_area_log + I(distance_protected_area_log * distance_protected_area_km5) +  I(distance_protected_area_log * distance_protected_area_km20)+
+      distance_cropland_2000_log + I(distance_cropland_2000_log * distance_cropland_2000_km5)  + I(distance_cropland_2000_log * distance_cropland_2000_km20)+
+      soilgrid_grouped + esa_cci_2000,
+    "f_proposal_log_nonlins" = area_accumulated_forest_loss_log ~
+      distance_mine_log + I(distance_mine_log * distance_mine_km5) + I(distance_mine_log * distance_mine_km20) +
+      elevation + slope +
+      pop_2000_log + area_forest_2000_log +
+      dist_road_log + I(dist_road_log * dist_road_km5) +  I(dist_road_log * dist_road_km50)+ 
+      dist_waterway_log + I(dist_waterway_log * dist_waterway_km10) + 
+      distance_protected_area_log + I(distance_protected_area_log * distance_protected_area_km20) + 
+      distance_cropland_2000_log + I(distance_cropland_2000_log * distance_cropland_2000_km10) +
+      soilgrid_grouped + esa_cci_2000,
+    "f_proposal_log_interactions_nonlins" = area_accumulated_forest_loss_log ~
+      distance_mine_log + I(distance_mine_log * distance_mine_km5) + I(distance_mine_log * distance_mine_km20)+
+      elevation + slope + I(elevation * slope) +
+      pop_2000_log + area_forest_2000_log + I(pop_2000_log * area_forest_2000_log) +
+      I(dist_road_log * pop_2000_log) + I(dist_road_log * distance_cropland_2000_log) + 
+      dist_road_log + I(dist_road_log * dist_road_km5) +  I(dist_road_log * dist_road_km50)+ 
+      dist_waterway_log + I(dist_waterway_log * dist_waterway_km10) + 
+      distance_protected_area_log + I(distance_protected_area_log * distance_protected_area_km20) + 
+      distance_cropland_2000_log + I(distance_cropland_2000_log * distance_cropland_2000_km10)  +
       soilgrid_grouped + esa_cci_2000
+    
   )
 
   source("code/3_models.R")
