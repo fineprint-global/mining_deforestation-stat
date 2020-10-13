f536833e6b7859671a753221b8df9e41df22e60f
SLuckeneder <sebastian.luckeneder@wu.ac.at>
2020-10-13 15:02:28 +0000

    initial commit of first tries

--- code/7_fitted_values.R
+++ code/7_fitted_values.R
@@ -99,3 +99,118 @@ ggplot2::ggsave("output/plots/coefs/proposal_1_fitted.png",
                 scale = 1, width = 300, height = 200, units = "mm")
 
 
+
+# fitted vs actual deforestation ------------------------------------------
+
+source("code/9_helpers.R")
+source("code/9_formulas.R")
+
+# case GHA
+
+# # look at the data # #
+
+# tbl_raw <- readRDS(paste0(path_in, file))
+# tbl <- prep_data(tbl_raw, has_forest = TRUE,
+#                  sub_eco = "Tropical", geom = TRUE)
+# tbl <- filter(tbl, area_accumulated_forest_loss > 0)
+# ggplot(tbl) +
+#   geom_sf(aes(fill = log(area_accumulated_forest_loss)), lwd = 0) +
+#   scale_fill_viridis_c() +
+#   labs(fill = "Log loss") +
+#   ggtitle(paste0("Forest loss (", get_iso(file), ")")) +
+#   theme(panel.grid.major = element_line(color = gray(0.5), linetype = "dashed", size = 0.2),
+#         panel.background = element_rect(fill = "transparent"))
+# 
+# plot(tbl$distance_mine, log(tbl$area_accumulated_forest_loss))
+
+# # load actual regression data and output # #
+countries <- read.csv("input/countries.csv")
+path_in <- "/mnt/nfs_fineprint/tmp/mining_def/"
+files <- paste0(countries$continent, "-", countries$iso, ".rds")
+files <- files[grep("AGO|BRA|COD|GUY|HND|IDN|MYS|NIC|ZMB", files)] 
+
+store1 <- list()
+store2 <- list()
+i <- 1
+file <- files[grep("HND", files)]
+
+for(i in seq(formulas)) {
+
+for(file in files){
+  
+  cat("Plotting", get_iso(file))
+  
+  load(paste0("output/reg_out_rda/lm_", get_iso(file), "_", names(formulas)[i], ".RData"))
+  summary(out_lm1)
+  load(paste0("output/reg_out_rda/y_", get_iso(file), "_", names(formulas)[i], ".RData"))
+  load(paste0("output/reg_out_rda/X_", get_iso(file), "_", names(formulas)[i], ".RData"))
+  
+  # # get fitted values # #
+  y_hat <- predict(out_lm1)
+  # plot(y, y_hat)
+  
+  # correct bias from log (see https://stats.stackexchange.com/questions/140713/making-predictions-with-log-log-regression-model)
+  adj <- sd(y - y_hat)
+  y_hat <- 0.5 * adj ^ 2 + y_hat
+  # points(y, y_hat, col=2)
+  # plot(y, y_hat)
+  # abline(0,1, col = "red")
+  p <- ggplot2::ggplot(data.frame(y, y_hat), aes(x = y, y = y_hat)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_abline(intercept = 0, slope = 1, color = "red") +
+    ggplot2::labs(title = paste(get_iso(file), names(formulas[i]))) 
+  store1[[file]] <- p
+  
+  X_cf <- X
+  X_cf[, "distance_mine_log"] <- mean(X[, "distance_mine_log"])
+  X_cf[, "I(distance_mine_log * distance_mine_km5)"] <- 0
+  X_cf[, "I(distance_mine_log * distance_mine_km25)"] <- 0
+  
+  coefs <- coef(out_lm1)
+  coefs[is.na(coefs)] <- 0
+  
+  y_cf <- cbind(1, X_cf) %*% coefs
+  adj <- sd(y - y_cf)
+  y_cf <- 0.5 * adj ^ 2 + y_cf
+  # points(y, y_cf, col=2)
+  p <- p +
+    ggplot2::geom_point(data = data.frame(y, y_cf), aes(x = y, y = y_cf), shape = 1, color = "green", fill = NA, alpha = 0.5) +
+    ggplot2::geom_abline(intercept = 0, slope = 1, color = "red") +
+    ggplot2::labs(title = paste(get_iso(file), names(formulas[i]))) 
+  store2[[file]] <- p
+  
+  # X_cf <- X
+  # X_cf[, "distance_mine_log"] <- max(X[, "distance_mine_log"])
+  # X_cf[, "I(distance_mine_log * distance_mine_km5)"] <- 0
+  # X_cf[, "I(distance_mine_log * distance_mine_km25)"] <- 0
+  # 
+  # coefs <- coef(out_lm1)
+  # coefs[is.na(coefs)] <- 0
+  # 
+  # y_cf <- cbind(1, X_cf) %*% coefs
+  # adj <- sd(y - y_cf)
+  # y_cf <- 0.5 * adj ^ 2 + y_cf
+  
+  # # points(y, y_cf, col=3)
+  # p <- p +
+  #   ggplot2::geom_point(data = data.frame(y, y_cf), aes(x = y, y = y_cf), shape = 1, color = "blue", fill = NA, alpha = 0.5) +
+  #   ggplot2::geom_abline(intercept = 0, slope = 1, color = "red")
+  
+  
+}
+# 
+# p_1 <- do.call(gridExtra::grid.arrange, c(store1, nrow=3))
+# ggplot2::ggsave("y_yhat_1_subs.png", 
+#                 plot = p_1, device = "png", 
+#                 path = paste0("./output/plots/y_yhat_scatter"),
+#                 scale = 1, width = 300, height = 300, units = "mm")
+
+p_2 <- do.call(gridExtra::grid.arrange, c(store2, nrow=3))
+ggplot2::ggsave("y_yhat_2_subs.png", 
+                plot = p_2, device = "png", 
+                path = paste0("./output/plots/y_yhat_scatter"),
+                scale = 1, width = 300, height = 300, units = "mm")
+
+}
+
+
