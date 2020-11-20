44a7fbb6509f364144c639279ea6f0d90beab17a
SLuckeneder <sebastian.luckeneder@wu.ac.at>
2020-11-20 11:56:30 +0000

    ned and modified latex outputs

--- code/9_print_coutnry_effects.R
+++ code/9_print_coutnry_effects.R
@@ -1,10 +1,19 @@
 
+library(tidyverse)
+library(xtable)
+library(countrycode)
+
 source("code/9_helpers.R")
+path_out <- "output/change_szenario/"
+countries <- c("AGO", "BRA", "COL", "COD", "CIV", "ECU", "GAB", "GHA", "GTM", "GIN", "GUY", "HND", "IND", "IDN", "KEN", "LBR", "MOZ", "MYS", "NIC", "PNG", "SLE", "TZA", "VEN", "VNM", "ZMB")
+model_name <- "f_base_log_minesize"
+coef_names <- c("distance_mine_log")
+
+
 country <- "IDN"
-model_name <- "f_vary_minesize"
-coef_names <- c("distance_mine_log", 
-                "I(distance_mine_log * distance_mine_km5)", 
-                "I(distance_mine_log * distance_mine_km25)")
+for(country in countries){
+  
+cat("\n Processing", country, "...")
 
 # load data used for regression -------------------------------------------
 
@@ -28,6 +37,8 @@ coeffs <- compare_models_merge(path = "output/txt",
 
 # extract dist_mine coeffs
 betas <- coeffs$lm_coef
+betas_025 <- coeffs$lm_coef - 2*coeffs$lm_se
+betas_975 <- coeffs$lm_coef + 2*coeffs$lm_se
 
 # set up data for computations --------------------------------------------
 
@@ -35,21 +46,77 @@ betas <- coeffs$lm_coef
 df <- data.frame(distance_mine = distance_mine,
                  def_m2 = def_m2)
 
-df <- df %>% dplyr::mutate(beta = ifelse(distance_mine < 5000, betas[1], NA))
-df <- df %>% dplyr::mutate(beta = ifelse(distance_mine < 25000 & is.na(beta), betas[1] + betas[2], beta))
-df <- df %>% dplyr::mutate(beta = ifelse(distance_mine >= 25000, betas[1] + betas[2] + betas[3],  beta))
+df <- df %>% dplyr::mutate(beta = betas, beta_025 = betas_025, beta_975 = betas_975)
+
+# df <- df %>% dplyr::mutate(beta = ifelse(distance_mine < 5000, betas[1], NA))
+# df <- df %>% dplyr::mutate(beta = ifelse(distance_mine < 25000 & is.na(beta), betas[1] + betas[2], beta))
+# df <- df %>% dplyr::mutate(beta = ifelse(distance_mine >= 25000, betas[1] + betas[2] + betas[3],  beta))
 
 # hypothetically shift all distances by 1 km and compute relative change
 df <- df %>%
   dplyr::filter(distance_mine > 1000) %>% # is there a better solution???
   dplyr::mutate(new_dist = distance_mine - 1000) %>%
-  dplyr::mutate(relative_change = (distance_mine - new_dist) / distance_mine * -1) 
+  dplyr::mutate(relative_change = (distance_mine - new_dist) / distance_mine * -1 * 100) 
 
-# calculate deforestation effect
+# calculate deforestation effect 95 confidence bound
 df <- df %>%
+  dplyr::mutate(beta_change_025 = relative_change * beta_025) %>%
   dplyr::mutate(beta_change = relative_change * beta) %>%
-  dplyr::mutate(deforestation_change = beta_change * def_m2)
+  dplyr::mutate(beta_change_975 = relative_change * beta_975) %>%
+  dplyr::mutate(deforestation_change_beta_025 = beta_change_025 * def_m2) %>%
+  dplyr::mutate(deforestation_change_beta = beta_change * def_m2) %>%
+  dplyr::mutate(deforestation_change_beta_975 = beta_change_975 * def_m2)
 
-sum(df$deforestation_change) / 1000000
+# write
+readr::write_csv(df, path = paste0(path_out, country, "_1km.csv"))
+
+cat("Done.")
+
+}
+
+
+
+# summarise szenario ------------------------------------------------------
+
+country <- "NIC"
+store <- list()
+
+for (country in countries){
+  
+  df <- readr::read_csv(file.path(path_out, paste0(country, "_1km.csv")))
+  df <- df %>% dplyr::mutate("country" = country)
+  store[[country]] <- df
+  
+}
+
+df <- dplyr::bind_rows(store)
+
+# note that lower and upper bound are switched
+df_summary <- df %>%
+  dplyr::group_by(country) %>%
+  dplyr::summarise(extra_deforestation_beta_025 = sum(deforestation_change_beta_975) / 1000000,
+                   extra_deforestation_beta = sum(deforestation_change_beta) / 1000000,
+                   extra_deforestation_beta_975 = sum(deforestation_change_beta_025) / 1000000) %>%
+  dplyr::arrange(-extra_deforestation_beta)
+
+
+# latex -------------------------------------------------------------------
+
+addtorow <- list()
+addtorow$pos <- list(0)
+addtorow$command <- "& \\multicolumn{3}{c}{Deforestation effect 1 km }"
+
+dfx <- df_summary %>% 
+  dplyr::select(-extra_deforestation_beta) %>%
+  dplyr::mutate(country = countrycode::countrycode(country, "iso3c", "eurostat.name")) %>%
+  `colnames<-`(c("Country", "95% lower", "95% upper"))
+print(xtable::xtable(dfx,
+                     align = "llrr",
+                     digits = 0,
+                     caption = "Estimated deforestation in a scenario where all observations were 1 km closer to a mine; lower and upper bound of 95\\% confidence interval.", 
+                     label = "tab:effects_1km"), 
+      format.args = list(big.mark = ",", decimal.mark = "."),
+      add.to.row=addtorow, include.rownames=FALSE, size="\\footnotesize")
+
 
 
--- code/9_print_results.R
+++ code/9_print_results.R
@@ -2,10 +2,10 @@
 library(dplyr)
 library(tidyr)
 library(xtable)
+library(countrycode)
 
 source("code/9_helpers.R")
 countries <- c("AGO", "BRA", "COL", "COD", "CIV", "ECU", "GAB", "GHA", "GTM", "GIN", "GUY", "HND", "IND", "IDN", "KEN", "LBR", "MOZ", "MYS", "NIC", "PNG", "SLE", "TZA", "VEN", "VNM", "ZMB")
-
 
 # base log minesize (main table) ------------------------------------------
 
@@ -50,7 +50,7 @@ df <- dplyr::left_join(A, B, by = "country") %>%
 ### export excel
 readr::write_csv(df, path = paste0("output/result_table_base_log_minesize.csv"))
 
-### latex
+### latex se below coeffs
 A <- df %>% dplyr::select(country, dm, N, N_tr_perc, R2) %>%
   dplyr::mutate(dm = as.character(round(dm, 3)))
 B <- df %>% dplyr::select(country, dm_se, N, N_tr_perc, R2) %>%
@@ -65,6 +65,17 @@ print(xtable::xtable(dfx,
                      digits = c(0, 0, 0, 0, 2, 2),
                      caption = "\\small Indirect deforestation effects induced by mining in 25 tropical countries; OLS estimates; standard errors in parentheses; see supplementary material for control variables", 
                      label = "tab:coefs"), include.rownames=FALSE, size="\\footnotesize")
+
+### latex se next to
+dfx <- df %>% dplyr::select(country, dm, dm_se, N, N_tr_perc, R2) %>%
+  dplyr::mutate(country = countrycode::countrycode(country, "iso3c", "eurostat.name")) %>%
+  `colnames<-`(c("Country", "DM", "SE", "N", "Share treated", "$R^2$"))
+print(xtable::xtable(dfx, 
+                     align = "llrrrrr",
+                     digits = c(0, 0, 3, 3, 0, 2, 2),
+                     caption = "\\small Indirect deforestation effects induced by mining in 25 tropical countries. Dependent variable is the log-transformed accumulated forest loss area between 2000 and 2019, DM (log) represents OLS estimates for the log-transformed distance to mine, and SE indicates standard errors. See supplementary material for control variables.", 
+                     label = "tab:coefs"), include.rownames=FALSE, size="\\footnotesize")
+
 
 # base log minesize (full appendix table) ---------------------------------
 
@@ -249,4 +260,81 @@ print(xtable::xtable(dfx,
                      digits = c(0, 0, 0, 0, 0, 2, 2),
                      caption = "\\small Indirect deforestation effects induced by mining in 25 tropical countries; OLS and logit estimates; standard errors in parentheses", 
                      label = "tab:compare_coefs"), include.rownames=FALSE, size="\\footnotesize")
+
+
+# dummy model -------------------------------------------------------------
+
+
+model_names <- c("f_dummy_minesize")
+coef_names <- c("km_insideTRUE", "km1_10TRUE", "km10_20TRUE", "km20_30TRUE", "km30_40TRUE", "km40_50TRUE", "min_area_1degree")
+column_names <- c("lm_coef", "lm_se")
+
+### load output csv 
+df <- compare_models_merge(path = "output/txt",
+                           files = paste0(paste0("coef_", rep(countries, length( model_names)), "_"), 
+                                          rep(model_names, each = length(countries)), ".csv"),
+                           coef_subs = coef_names)
+
+info <- compare_models_info(path = "output/txt",
+                            files = paste0(paste0("info_", rep(countries, length( model_names)), "_"), 
+                                           rep(model_names, each = length(countries)), ".csv"))
+
+### tidy data
+# select columns
+df <- df %>% dplyr::select("country", "model", "vars", all_of(column_names))
+
+# spread by coef
+A <- df %>% 
+  dplyr::select(-lm_se, -model) %>%
+  tidyr::spread(key = "vars", value = "lm_coef")
+
+B <- df %>% 
+  dplyr::select(-lm_coef, -model) %>%
+  tidyr::spread(key = "vars", value = "lm_se") %>%
+  `colnames<-`(c("country", paste0(colnames(A)[-1], "_se")))
+
+C <- info %>%
+  dplyr::select(country, N, N_tr_matched, R2) %>%
+  dplyr::mutate(N_tr_perc = N_tr_matched / N)
+
+# merge
+df <- dplyr::left_join(A, B, by = "country") %>%
+  dplyr::left_join(C, by = "country") %>%
+  dplyr::select(country, 
+                "km_insideTRUE", "km_insideTRUE_se", "km1_10TRUE", "km1_10TRUE_se", "km10_20TRUE", "km10_20TRUE_se", 
+                "km20_30TRUE", "km20_30TRUE_se", "km30_40TRUE", "km30_40TRUE_se", "km40_50TRUE", "km40_50TRUE_se", "min_area_1degree", "min_area_1degree_se",
+                N, N_tr_perc, R2)
+
+### export excel
+readr::write_csv(df, path = paste0("output/result_table_dummy_minesize.csv"))
+
+### latex
+A <- df %>% dplyr::select(country, "km_insideTRUE", "km1_10TRUE", "km10_20TRUE", "km20_30TRUE", "km30_40TRUE", "km40_50TRUE", "min_area_1degree", N, N_tr_perc, R2) %>%
+  dplyr::mutate(km_insideTRUE = as.character(round(km_insideTRUE, 3)),
+                km1_10TRUE = as.character(round(km1_10TRUE, 3)),
+                km10_20TRUE = as.character(round(km10_20TRUE, 3)),
+                km20_30TRUE = as.character(round(km20_30TRUE, 3)),
+                km30_40TRUE = as.character(round(km30_40TRUE, 3)),
+                km40_50TRUE = as.character(round(km40_50TRUE, 3)),
+                min_area_1degree = as.character(round(min_area_1degree, 3)))
+B <- df %>% dplyr::select(country, "km_insideTRUE_se", "km1_10TRUE_se", "km10_20TRUE_se", "km20_30TRUE_se", "km30_40TRUE_se", "km40_50TRUE_se", "min_area_1degree_se", N, N_tr_perc, R2) %>%
+  dplyr::mutate(km_insideTRUE_se = as.character(paste0("(", round(km_insideTRUE_se, 3), ")")),
+                km1_10TRUE_se = as.character(paste0("(", round(km1_10TRUE_se, 3), ")")),
+                km10_20TRUE_se = as.character(paste0("(", round(km10_20TRUE_se, 3), ")")),
+                km20_30TRUE_se = as.character(paste0("(", round(km20_30TRUE_se, 3), ")")),
+                km30_40TRUE_se = as.character(paste0("(", round(km30_40TRUE_se, 3), ")")),
+                km40_50TRUE_se = as.character(paste0("(", round(km40_50TRUE_se, 3), ")")),
+                min_area_1degree_se = as.character(paste0("(", round(min_area_1degree_se, 3), ")")))
+B <- B %>% dplyr::mutate(N = NA, N_tr_perc = NA, R2 = NA) %>%
+  `colnames<-`(colnames(A))
+dfx <- dplyr::bind_rows(A, B) %>% dplyr::arrange(country) %>%
+  `colnames<-`(c("Country", "Within", "1-10 km", "10-20 km", "20-30 km", "30-40 km", "40-50 km", "Mine area", "Observations", "Share treated", "$R^2$"))
+dfx$Country[seq(2, nrow(dfx), 2)] <- NA
+print(xtable::xtable(dfx, 
+                     align = "llcccccccrrr",
+                     digits = c(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2),
+                     caption = "\\small OLS estimates; standard errors in parentheses; dummy variable for buffered distances to mine", 
+                     label = "tab:dummies"), 
+      floating = TRUE, floating.environment = "sidewaystable", include.rownames=FALSE, size="\\footnotesize")
+
 
